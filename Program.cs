f92d61e32ec01b46ce44b878eb7ac0f893b3f23a
Justin Rodriguez <jjrod297@gmail.com>
2025-02-13 21:08:55 -0800

    Revised Coding Assignment 4 to showcase my skills in implementing methods inside my code.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 // Course: COMP-003A
 // Faculty: Jonathan Cruz 
 // Purpose: Inventory management application with a minimum of 10 elements in the collection.
+using System.Diagnostics.Metrics;
+
 namespace COMP003A.CodingAssignment4
 {
     internal class Program
@@ -16,16 +18,13 @@ namespace COMP003A.CodingAssignment4
             int count = 0; // To keep track of how many veggies are in the array
             int input = 0; // Record user input in menu
 
-            Console.WriteLine("Welcome to the Inventory Management System!");
+            // Called my first method and wanted to start small
+            GreetUser();
 
             while (true)
             {
-                Console.WriteLine("Inventory Management System Menu:");
-                Console.WriteLine("1. Add a Product");
-                Console.WriteLine("2. Update Product Quantity");
-                Console.WriteLine("3. View Inventory Summary");
-                Console.WriteLine("4. Exit");
-                Console.Write("Enter your choice: ");
+                // Called a method with multiple strings
+                DisplaySystemMenu();
 
                 try
                 { 
@@ -46,78 +45,19 @@ namespace COMP003A.CodingAssignment4
 
                 if (input == 1)
                 {
-                    Console.Write("Enter product name: ");
-
-                    try
-                    {
-                        veggieName = Console.ReadLine();
-
-                        // Checking if the input is a word and not a number
-                        if (int.TryParse(veggieName, out int parsedNumber))
-                        {
-                            throw new Exception("Invalid input. Please enter a name.");
-                        }
-
-                    }
-                    catch (Exception)
-                    {
-                        throw;
-                    }
-
-                    Console.Write("Enter product quantity: ");
-
-                    try
-                    {
-                        veggieQuantity = int.Parse(Console.ReadLine());
-                    }
-                    catch (Exception)
-                    {
-                        throw;
-                    }
-
-                    if (count < veggieNames.Length) // This if statement is going to check if there is enough room to hold products/quantities in the array
-                    {
-                        veggieNames[count] = veggieName;
-                        veggieQuantities[count] = veggieQuantity;
-                        count++;
-                        Console.WriteLine("Product added successfully!");
-                    }
-                    else // If array is full, a messsage will display saying "Inventory is full!"
-                    {
-                        Console.WriteLine("Inventory is full. Cannot add more products.");
-                    }
-
+                    // Decided to up it up a little and call a method with variables inside of the param
+                    AddProduct(veggieNames, veggieQuantities, veggieName, veggieQuantity, count);
                 }
                 else if (input == 2)
                 {
-                    Console.Write("Enter product name: ");
-                    string productInput = Console.ReadLine();
-
-                    // Now we are going to find the index of the product name in the array
-                    int index = Array.IndexOf(veggieNames, productInput);
-
-                    if (index != -1) // This if statement is going to check if the product name was found
-                    {
-                        Console.WriteLine($"Current quantity of {productInput}: {veggieQuantities[index]}");
-                        Console.Write("Enter a new quantity: ");
-
-                        try
-                        {
-                            int newveggieQuantity = int.Parse(Console.ReadLine());
-                            veggieQuantities[index] = newveggieQuantity; // Updated the quantity 
-                            Console.WriteLine("Product quantity updated successfully!");
-                        }
-                        catch (Exception)
-                        {
-                            throw;
-                        }
-                    }
-                    else
-                    {
-                            Console.WriteLine("Product does not exist.");
-                    }
+                    // Same again here, getting used to calling methods with variables inside of the param
+                    UpdateProduct(veggieNames, veggieQuantities);
                     
                 }
+                /*
+                I'm not going to use a another method here because I want to showcase a value method inside.
+                Otherwise I would have to keep everything symmetrical with the other input conditions.
+                */
                 else if (input == 3)
                 {
                     int totalveggieNames = 0;
@@ -133,8 +73,8 @@ namespace COMP003A.CodingAssignment4
                         totalveggieQuantities += veggieQuantities[i];
                     }
 
-                    // Here we calculate the average quantity of all the products
-                    double averageQuantity = (totalveggieNames > 0) ? (double)totalveggieQuantities / totalveggieNames : 0;
+                    // Here I wanted to show a value method in use
+                    double averageQuantity = CalculateAverage(totalveggieNames, totalveggieQuantities);
 
                     // Display all products, quantities, and average
                     Console.WriteLine($"Total Products: {totalveggieNames}");
@@ -149,6 +89,100 @@ namespace COMP003A.CodingAssignment4
 
             }
 
+        }
+
+        static void GreetUser()
+        {
+            Console.WriteLine("Welcome to the Inventory Management System!");
+        }
+
+        static void DisplaySystemMenu()
+        {
+            Console.WriteLine("Inventory Management System Menu:");
+            Console.WriteLine("1. Add a Product");
+            Console.WriteLine("2. Update Product Quantity");
+            Console.WriteLine("3. View Inventory Summary");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
+        }
+
+        static void AddProduct(string[] veggieNames, int[] veggieQuantities, string veggieName, int veggieQuantity, int count)
+        {
+            Console.Write("Enter product name: ");
+
+            try
+            {
+                veggieName = Console.ReadLine();
+
+                // Checking if the input is a word and not a number
+                if (int.TryParse(veggieName, out int parsedNumber))
+                {
+                    throw new Exception("Invalid input. Please enter a name.");
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            Console.Write("Enter product quantity: ");
+
+            try
+            {
+                veggieQuantity = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            if (count < veggieNames.Length) // This if statement is going to check if there is enough room to hold products/quantities in the array
+            {
+                veggieNames[count] = veggieName;
+                veggieQuantities[count] = veggieQuantity;
+                count++;
+                Console.WriteLine("Product added successfully!");
+            }
+            else // If array is full, a messsage will display saying "Inventory is full!"
+            {
+                Console.WriteLine("Inventory is full. Cannot add more products.");
+            }
+        }
+
+        static void UpdateProduct(string[] veggieNames, int[] veggieQuantities)
+        {
+            Console.Write("Enter product name: ");
+            string productInput = Console.ReadLine();
+
+            // Now we are going to find the index of the product name in the array
+            int index = Array.IndexOf(veggieNames, productInput);
+
+            if (index != -1) // This if statement is going to check if the product name was found
+            {
+                Console.WriteLine($"Current quantity of {productInput}: {veggieQuantities[index]}");
+                Console.Write("Enter a new quantity: ");
+
+                try
+                {
+                    int newveggieQuantity = int.Parse(Console.ReadLine());
+                    veggieQuantities[index] = newveggieQuantity; // Updated the quantity 
+                    Console.WriteLine("Product quantity updated successfully!");
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Product does not exist.");
+            }
+        }
+
+        static double CalculateAverage(int totalveggieNames, int totalveggieQuantities)
+        {
+            return (totalveggieNames > 0 ? (double)totalveggieQuantities / totalveggieNames: 0);
         }
     }
 }
